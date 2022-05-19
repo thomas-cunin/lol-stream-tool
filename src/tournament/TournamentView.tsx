a4ad1b9720c687517999c71fedb4b4f71b29918a
thomas-cunin <thomas.cunin@outlook.fr>
2022-05-19 19:30:17 +0200

    fix build fail

--- src/tournament/TournamentView.tsx
+++ src/tournament/TournamentView.tsx
@@ -1,5 +1,86 @@
-export default function TournamentView() {
+import TournamentPlayer from "./components/TournamentPlayer";
+import {useState} from "react";
+import axios from "axios";
+// @ts-ignore
+export default function TournamentView({id, name = 'Untitled', date}) {
+    const [championsLibrary, setChampionsLibrary] = useState([
+        {id:'test', value:'Pense à charger la liste'}
+    ])
+    const [players, setPlayers] = useState([
+        {
+            username: 'Raiz',
+            champions: ['Akali', 'Ahri']
+        },
+        {
+            username: 'Citronatorix',
+            champions: ['Urgot', 'Darius']
+        }
+        ]
+    );
+    const updateChampionsLibrary = function updateChampionsLibrary(){
+        axios.get("https://ddragon.leagueoflegends.com/cdn/12.9.1/data/fr_FR/champion.json")
+            .then(response => {
+                var championsLib = [];
+                for (const [key, value] of Object.entries(response.data.data)) {
+                    championsLib.push({id:`${key}`, value:`${key}`})
+                }
+                console.log(championsLib);
+                setChampionsLibrary(championsLib);
+            })
+            .catch(function(error) {
+                // Error
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    // console.log(error.response.data);
+                    // console.log(error.response.status);
+                    // console.log(error.response.headers);
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an instance of XMLHttpRequest in the
+                    // browser and an instance of
+                    // http.ClientRequest in node.js
+                    console.log(error.request);
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log('Error', error.message);
+                }
+                console.log(error.config);
+            });
+    }
+    const updateUsername = function updateUsername(i:number,newUsername:string) {
+        let mapped = players.map((player, index)=>{return index == i ? { ...player, username: newUsername } : { ...player}})
+        console.log(players);
+
+        setPlayers(mapped);
+        console.log(players);
+    };
+    const removeChampion = function removeChampion(i:number, champion:string){
+        let mapped = players.map((player, index)=>{return index == i ? {...player, champions: player.champions.filter(c => c !== champion)} : {...player}});
+        setPlayers(mapped);
+    }
+    const addChampion = function addChampion(i:number, champion:string){
+        let mapped = players.map((player, index)=>{return index == i ? {...player, champions: player.champions.concat(champion)} : {...player}});
+        setPlayers(mapped);
+    }
+    function addPlayer(uname:string) {
+        const newPlayer = {
+            username:uname,
+            champions:[]
+        };
+        setPlayers(players.concat(newPlayer));
+    }
     return (
-        <div></div>
+        <div className={'tournament-view'}>
+            <button onClick={updateChampionsLibrary}>Charger la liste des champions</button>
+            <div className={"tournament-header"}><span>{name}</span></div>
+            <button onClick={()=>{return addPlayer('New')}}>Ajouter un joueur</button>
+            <div className={"tournament-players-list"}>
+                {players.map((item, index)=>{
+                    return <TournamentPlayer championsLibrary={championsLibrary} addChampion={addChampion} updateUsername={updateUsername} removeChampion={removeChampion} player={item} key={index}  index={index}/>
+                })}
+            </div>
+
+        </div>
     )
 }
--- src/tournament/components/TournamentPlayer.tsx
+++ src/tournament/components/TournamentPlayer.tsx
@@ -1,7 +1,52 @@
-export default function TournamentView() {
-    return (
-        <div className={'tournament-view'}>
 
+import DatalistInput, {useComboboxControls} from "react-datalist-input";
+import axios from "axios";
+// @ts-ignore
+export default function TournamentPlayer({player, index, updateUsername, removeChampion, addChampion, championsLibrary}) {
+    const { setValue, value } = useComboboxControls({ isExpanded:false,initialValue: '' });
+    return (
+        <div className={'tournament-player'}>
+            <div className={"player-infos"}><span className={'player-username'}>{player.username}</span></div>
+
+            <div className={"player-champions-list"}>
+                {player.champions.map((chammpion: string) => {
+                    return <img src={"http://ddragon.leagueoflegends.com/cdn/12.9.1/img/champion/" + chammpion + ".png"}
+                                alt=""/>
+                })}
             </div>
+            <div className={"player-config"}>
+                <label className={"dropdown"}>
+
+
+                    <div className="dd-button">
+                        Edit
+                    </div>
+
+                    <input type="checkbox" className="dd-input" id="test"/>
+
+                    <ul className="dd-menu">
+                        <li>Pseudo: <input type="text" value={player.username}
+                                           onChange={(e) => updateUsername(index, e.target.value)}/></li>
+                        <li className="divider"></li>
+                        {player.champions.map((chammpion: string) => {
+                            return <li>{chammpion} <button onClick={(e) => removeChampion(index, chammpion)}>Suppr</button></li>
+                        })}
+                        <DatalistInput
+                            value={value}
+                            setValue={setValue}
+                            placeholder="Urgot"
+                            label="Ajouter un champion"
+                            onSelect={(item) => {
+                                addChampion(index, item.value);
+                                setValue('');
+                            }}
+                            items={championsLibrary}
+                        />
+                    </ul>
+
+                </label>
+            </div>
+
+        </div>
     )
 }
